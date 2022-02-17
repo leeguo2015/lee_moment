d307e3fe718a53880a087dd5eac2bc5949656965
leeguo <leeguo2015@163.com>
2022-02-17 23:28:48 +0800

    relationship

--- moment_web/src/components/HelloWorld.vue
+++ moment_web/src/components/HelloWorld.vue
@@ -1,13 +1,16 @@
 <template>
-  <h1>{{ msg }}</h1>
+<div>
+    <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ data.counter }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
   <p> {{data.counter}}</p>
   <p> {{count}}</p>
+</div>
 </template>
  
 <script>
 import { reactive} from 'vue'
+
 
 export default {
   name: 'HelloWorld',
@@ -28,3 +31,5 @@ export default {
   // }
 }
 </script>
+
+50864987
