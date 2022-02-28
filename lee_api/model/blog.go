6166ca2e5d76946bf2a249fce0e10bfaea0d2ad2
leeguo <leeguo2015@163.com>
2022-02-28 23:20:01 +0800

    blog models.

--- lee_api/model/blog.go
+++ lee_api/model/blog.go
@@ -1,1 +1,23 @@
 package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type (
+	TagType struct {
+		gorm.Model
+		Name string
+	}
+	Tags struct {
+		gorm.Model
+		tagType TagType `gorm:"foreignkey:TagTypeID"`
+		BlogId  Blog
+	}
+
+	Blog struct {
+		gorm.Model
+		Uuid   string `json:"uuid" gorm:"column:uuid;type:varchar(100);"`
+		UserId User   `gorm:"foreignkey:UserRefer"`
+	}
+)
